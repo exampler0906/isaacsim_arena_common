b9340f35de2d14e137bef1ef16a34e960015995d
exampler0906 <kanweipeng@qq.com>
2026-03-31 17:05:44 +0800

    update command；update profile

--- object_position.py
+++ object_position.py
@@ -1,56 +1,75 @@
-# 物体相对于 base 系的位置偏移
+# 机械臂抓取位置相对于物体坐标系的偏移
 object_offect_xyz_dict = {
     "cracker_box_small": {"x": -0.25, "y": -0.01, "z": 0.0},
     "custom_box": {"x": -0.10, "y": 0.0, "z": -0.01},
+    "custom_package": {"x": -0.18, "y": 0.0, "z": -0.01},
 }
 
 # 物体相对于初始坐标系的旋转
 object_rotation_dict = {
     "cracker_box_small": (0.5, 0.5, -0.5, 0.5),
     "custom_box": (1.0, 0.0, 0.0, 0.0),
+    "custom_package": (1.0, 0.0, 0.0, 0.0),
 }
   
+# 物体在不同background下的初始位置
 object_init_position = {
     "packing_table":{"cracker_box_small": (0.7, 0.2, 0.15),},
-    "office_204":{"custom_box": (0.2, 0.03, 0.7),},}
+    "office_204":{"custom_package": (0.2, 0.03, 0.7),"custom_box": (0.2, 0.03, 0.7),},
+    }
 
+
+# 物体在不同background下的目标位置
 object_destination_position = {
-    "packing_table":(0.35,-0.35,0.30),
-    "office_204":(0.18,-0.20,0.70),
+    "packing_table":{"cracker_box_small": (0.35,-0.35,0.30),},
+    "office_204":{"custom_box": (0.18,-0.20,0.72),"custom_package": (0.08,-0.20,0.72), },
 }
 
-#
+# 物体在不同background下的随机位置范围
 object_position_random_range = {
-    "office_204":{"custom_box" : {"x":(0.2, 0.3), "y":(0.0, 0.05), "z":0.70 },},
+    "office_204":{"custom_box" : {"x":(0.2, 0.3), "y":(0.0, 0.05), "z":0.70 },
+                  "custom_package" : {"x":(0.2, 0.3), "y":(0.0, 0.05), "z":0.70 },},
     "packing_table":{"cracker_box_small" : {"x":(0.6, 0.8), "y":(0.15, 0.25), "z":0.15},},
 }
 
+# 夹爪闭合缩放系数，临时解决夹爪太松的问题
+object_gripper_scale = {
+    "custom_box": 0.85,
+    "custom_package": 0.85,
+}
 
+# 场景相机 prim_path
 scene_cam_prim_path = {
     "office_204" : "office_204/camera/Camera/base_link/camera_bottom_screw_frame/camera_link/camera_color_frame/SceneCam",
     "packing_table" : "SceneCam"
 }
 
+# 场景相机位置
 scene_cam_position = {
     "office_204" : (0.1, 0.0, 0.02),
     "packing_table" :(0.2, -0.09245, 1.4)
 }
 
+# 场景相机焦距
 scene_cam_focal_length = {
-    "office_204" : 15,
+    "office_204" : 18,
     "packing_table" :8
 }
 
+# 场景相机旋转
 scene_cam_rotation = {
     "office_204" : (0.5, 0.5, -0.5, -0.5),
     "packing_table" :(0.70711, 0.0, 0.0, -0.70711)
 }
 
 
+# 机械臂在不同background下的初始位置
 embodiment_init_position = {
     "packing_table":{"big_custom" : (-0.25, 0.0, 0.0),},
-    "office_204":{"custom" : (-0.25, 0.0, 0.65),}
+    "office_204":{"custom" : (-0.27, -0.24, 0.65),}
 }
+
+# 机械臂在不同background下的初始旋转
 embodiment_init_rotation = {
     "packing_table":{"big_custom" : (1.0, 0.0, 0.0, 0.0),},
     "office_204":{"custom" : (1.0, 0.0, 0.0, 0.0),}
